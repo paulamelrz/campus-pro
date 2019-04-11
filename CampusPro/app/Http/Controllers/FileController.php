e890cd9607f5f501d4e1d5ae0696119f30736575
pHantompX3 <wayden13@gmail.com>
2019-04-11 02:07:18 -0400

    More adjustments to file controller

--- CampusPro/app/Http/Controllers/FileController.php
+++ CampusPro/app/Http/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class FileController extends Controller
 {
@@ -12,7 +13,14 @@ class FileController extends Controller
         /**
          * @var UploadedFile
          */
+
         $file = $request->file('file');
-        $file->storePubliclyAs('upload','filename','public');
+        $name = Input::file('file')->getClientOriginalName();
+        $path = "CampusPro\\public\\upload\\".$name;
+        $path = str_replace("\\", DIRECTORY_SEPARATOR, $path);
+        $extension = Input::file('file')->getClientOriginalExtension();
+        $size = Input::file('size')->getSize();
+        $owner = "the current user";
+        $file->storePubliclyAs('upload',$name,'public');
     }
 }
