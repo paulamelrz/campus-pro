8f69e7290c9caaeea1b6139bdcbd6827dbd8f7d1
pHantompX3 <wayden13@gmail.com>
2019-04-11 02:13:56 -0400

    all neccesary items for database update

--- CampusPro/app/Http/Controllers/FileController.php
+++ CampusPro/app/Http/Controllers/FileController.php
@@ -19,8 +19,9 @@ class FileController extends Controller
         $path = "CampusPro\\public\\upload\\".$name;
         $path = str_replace("\\", DIRECTORY_SEPARATOR, $path);
         $extension = Input::file('file')->getClientOriginalExtension();
-        $size = Input::file('size')->getSize();
+        $size = Input::file('file')->getSize();
         $owner = "the current user";
+        $owner_type = "the current user's type";
         $file->storePubliclyAs('upload',$name,'public');
     }
 }
