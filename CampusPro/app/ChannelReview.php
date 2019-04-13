17a487151006c2c1c8608059f0ebfe3554fd0b47
hackergirl1 <paula.meleroramirez@mycavehill.uwi.edu>
2019-04-13 02:03:00 -0400

    review migration up
    please do a php artisan migrate

--- CampusPro/app/ChannelReview.php
+++ CampusPro/app/ChannelReview.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChannelReview extends Model
 {
-    //
+    public $table='channel_reviews';
+
+    public $fillable = ['channels_id', 'stu_id', 'title', 'comment', 'stars', 'likes'];
+
+    //relatinships
+    public function channel(){
+        return $this->belongsTo('App\Channel');
+    }
 }
