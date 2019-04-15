af78e84a112669813fdd3ac6dc072ab617367656
pHantompX3 <wayden13@gmail.com>
2019-04-15 00:54:40 -0400

    Corrections to Discussion Migrations

--- CampusPro/app/DiscussionReplies.php
+++ CampusPro/app/DiscussionReplies.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class DiscussionReplies extends Model
 {
 
+    public $table = 'discussion_replies';
+    public $fillable = ['thread_id','tutor_id','body'];
+
 
     public function topic(){
         return $this->belongsto('App\DiscussionThread');
