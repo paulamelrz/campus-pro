5f8302cd2d58205f90dfbf2f44f8a1eca7fc023e
pHantompX3 <wayden13@gmail.com>
2019-04-16 14:41:12 -0400

    Update 2019_04_15_014408_create_discussion_threads_table.php

--- CampusPro/database/migrations/2019_04_15_014408_create_discussion_threads_table.php
+++ CampusPro/database/migrations/2019_04_15_014408_create_discussion_threads_table.php
@@ -24,27 +24,13 @@ class CreateDiscussionThreadsTable extends Migration
                 $table->timestamps();
 
 
-
-            $table->bigIncrements('id');
-            $table->unsignedInteger('student_id');
-            $table->unsignedInteger('channel_id');
-            $table->unsignedInteger('replies_count')->default(0);
-            $table->string('title');
-            $table->text('body');
-            $table->unsignedInteger('best_reply_id')->nullable();
-            $table->boolean('flag')->default(false);
-            $table->timestamps();
-
-           
-                $table->foreign('best_reply_id')->references('id')->on('discussion_replies')->onDelete('set null');
-
-
                 $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
 
                 $table->foreign('channel_id')->references('channel_id')->on('channels')->onDelete('cascade');
 
-    });
-}
+        });
+
+    }
 
     /**
      * Reverse the migrations.
